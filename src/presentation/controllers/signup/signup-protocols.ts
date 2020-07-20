24a11cc91a4cbe286033726a1da7ef14a91c78be
danieljesus <daniel.j.rodrigues06@gmail.com>
2020-07-20 18:17:08 -0300

    refactor: move all signup protocols to a single file

---
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -0,0 +1,4 @@
+export * from '../../protocols'
+export * from '../../protocols/emailValidator'
+export * from '../../../domain/useCases/addAcounts'
+export * from '../../../domain/models/account'
--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -1,8 +1,6 @@
 import { SignUpController } from './signup'
-import { EmailValidator } from '../protocols'
-import { MissingParamError, InvalidParamError, ServerError } from '../errors'
-import { AccountModel } from '../../domain/models/account'
-import { AddAccountModel, AddAccount } from '../../domain/useCases/addAcounts'
+import { MissingParamError, InvalidParamError, ServerError } from '../../errors'
+import { EmailValidator, AddAccountModel, AddAccount, AccountModel } from './signup-protocols'
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
