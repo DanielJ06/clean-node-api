c54574babdb8aff9b4deb9cda78d80f6574573ae
danieljesus <daniel.j.rodrigues06@gmail.com>
2020-07-23 15:29:00 -0300

    test: ensure EmailValidatorAdapter calls validator with valid email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -20,4 +20,11 @@ describe('EmailValidator adapter', () => {
     const isValid = sut.isValid('valid_email@gmail.com')
     expect(isValid).toBe(true)
   })
+
+  test('Should call EmailValidator with valid email', () => {
+    const sut = new EmailValidatorAdapter()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isValid('any_email@gmail.com')
+    expect(isEmailSpy).toHaveBeenCalledWith('any_email@gmail.com')
+  })
 })
